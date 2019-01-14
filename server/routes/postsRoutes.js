5eb5403ac9c324b3ca7348e3771535cbefbd7d8d
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2019-01-14 15:21:29 +1300

    api linked to test on postman

---
+++ server/routes/postsRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const request = require('superagent')
+
+router.get('/', (req, res) => {
+  request.get('https://jsonplaceholder.typicode.com/posts')
+    .then(ApiRes => {
+      res.json(ApiRes.text)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ const express = require('express')
 const path = require('path')
 const server = express()
 
+const postsRoutes = require('./routes/postsRoutes');
 
 server.use(express.static(path.join(__dirname, '..', 'public')))
 server.use(express.json())
+
+server.use('/api', postsRoutes)
 
 server.get('/test', (req, res) => {
 res.send('test test test test')
