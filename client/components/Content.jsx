eb6f72a90ebdfa3b437cf9bbd202872b83c8724a
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2019-02-15 17:38:56 +1300

    add a game component front end

--- client/components/Content.jsx
+++ client/components/Content.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Rules from './Rules';
+import Game from './Game';
 
 const Content = props => {
   return (
     <div> 
-      <h4 className="text-success">Game on !{props.title}</h4>
+      <h4 className="text-success">Game on !</h4>
       <p>
       Play cards against humanity, because it is fun. But you is funnier ? You ? or the computer ?
       </p>
+      <Game/>
       <Rules/>
     </div>
   );
---
+++ client/components/Game.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Game = props => {
+  return (
+    <div className="container-game">
+      <div id='game'>
+        <h4 className="text-success">PLAY</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
