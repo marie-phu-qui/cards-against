4993cfde023c504287f024bd01a3a472b11289df
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2019-02-15 18:30:55 +1300

    need to fix the route /game on the url - need to toggle the rules set

--- client/components/Content.jsx
+++ client/components/Content.jsx
@@ -1,18 +1,55 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
 import Rules from './Rules';
 import Game from './Game';
 
-const Content = props => {
+class Content extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      display: {}
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick(e) {
+    e.preventDefault();
+    console.log('Display the clicked component !');
+    console.log(this.state);
+    return(
+      this.setState({
+        display: {
+          'rules' : true,
+        }
+      })
+    )
+  }
+  render(){
   return (
-    <div> 
-      <h4 className="text-success">Game on !</h4>
-      <p>
-      Play cards against humanity, because it is fun. But you is funnier ? You ? or the computer ?
-      </p>
-      <Game/>
-      <Rules/>
-    </div>
+    <Router>
+      <div> 
+        <h4 className="text-success">Game on !</h4>
+        <p>
+        Play cards against humanity, because it is fun. But you is funnier ? You ? or the computer ?
+        </p>
+
+        <div id='game' className="text-success">
+          <h4>
+            <Link to="/game">Click to START</Link>
+          </h4>
+        </div>
+
+        <div onClick={this.handleClick}>
+        <a id='rules' className="text-success" href='/'>See the rules</a>{this.state.display.rules == true && <Rules />}
+        </div>
+
+        <Route exact path="/game" render={() => <Game />} />
+      </div>
+    </Router>
+
   );
+  }
 };
 
 
--- client/components/Game.jsx
+++ client/components/Game.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Card from "./Card";
 
 const Game = props => {
   return (
     <div className="container-game">
       <div id='game'>
-        <h4 className="text-success">PLAY</h4>
+        <h4 className="text-success">PLAY will happen here</h4>
+        <Card/>
       </div>
     </div>
   );
