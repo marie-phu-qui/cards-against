94d52dd02437f836759585c5e5133a1fe291f44e
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2019-02-18 12:21:35 +1300

    updated routes

--- server/server.js
+++ server/server.js
@@ -2,14 +2,8 @@ const express = require('express')
 const path = require('path')
 const server = express()
 
-const postsRoutes = require('./routes/postsRoutes');
-const contentRoutes = require('./routes/contentRoutes');
-
 server.use(express.static(path.join(__dirname, '..', 'public')))
 server.use(express.json())
-
-server.use('/api', postsRoutes)
-server.use('/content', contentRoutes)
 
 
 server.get('/test', (req, res) => {
